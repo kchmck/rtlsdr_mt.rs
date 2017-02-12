6b62712ab7a29f5c50049c6fbc27a46a7eea4e6b
Mick Koch <mick@kochm.co>
2017-02-11 20:49:50 -0500

    Add an example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
 //! This crate provides a high-level interface to the RTL-SDR that separates controlling
 //! the device and reading samples, for integration into multithreaded applications.
+//!
+//! ## Example
+//!
+//! This example reads incoming samples, printing the first I/Q pair, in the main thread
+//! while incrementing the receive frequency by 1kHz every second in a subthread.
+//!
+//! ```rust,no_run
+//! let (mut ctl, mut reader) = rtlsdr::open(0).unwrap();
+//!
+//! ctl.enable_agc().unwrap();
+//! ctl.set_ppm(-2).unwrap();
+//! ctl.set_center_freq(774_781_250).unwrap();
+//!
+//! std::thread::spawn(move || {
+//!     loop {
+//!         let next = ctl.center_freq() + 1000;
+//!         ctl.set_center_freq(next).unwrap();
+//!
+//!         std::thread::sleep(std::time::Duration::from_secs(1));
+//!     }
+//! });
+//!
+//! reader.read_async(4, 32768, |bytes| {
+//!     println!("i[0] = {}", bytes[0]);
+//!     println!("q[0] = {}", bytes[1]);
+//! }).unwrap();
+//! ```
 
 #![feature(conservative_impl_trait)]
 
